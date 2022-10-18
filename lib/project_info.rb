4d70f8438de786ec58312387775b784354d15e0f
KaiyiChu <s28238385@gmail.com>
2022-10-18 23:18:48 +0800

    Complete rewrite project_library

--- lib/project_info.rb
+++ lib/project_info.rb
@@ -4,31 +4,32 @@ require 'http'
 require 'yaml'
 
 config = YAML.safe_load(File.read('../config/secrets.yml'))
+SEARCH_KEY_WORD = 'taylor%20swift'
+SEARCH_RESULT_NUM = '5'
 
-def fb_api_path(path, config)
-  "https://graph.facebook.com/v15.0/#{path}&access_token=#{config['ACCESS_TOKEN']}"
+def yt_api_path(search, config, result_num)
+  "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{search}&key=#{config['ACCESS_TOKEN']}&type=video&maxResults=#{result_num}"
 end
 
-def call_fb_url(url)
+def call_yt_url(url)
   HTTP.headers('Accept' => 'application/json').get(url)
 end
 
-fb_response = {}
-fb_results = {}
+yt_response = {}
+yt_results = {}
 
 ## HAPPY project request
-project_url = fb_api_path('me?fields=name%2Cbirthday%2Cemail%2Cfriends', config)
-fb_response[project_url] = call_fb_url(project_url)
-project = fb_response[project_url].parse
+project_url = yt_api_path(SEARCH_KEY_WORD, config, SEARCH_RESULT_NUM)
+yt_response[project_url] = call_yt_url(project_url)
+project = yt_response[project_url].parse
 
-fb_results['name'] = project['name']
-fb_results['birthday'] = project['birthday']
-fb_results['email'] = project['email']
-fb_results['friends_total_count'] = project['friends']['summary']['total_count']
+yt_results['kind'] = project['kind']
+yt_results['regionCode'] = project['regionCode']
+yt_results['items'] = project['items']
 
 # BAD project request
-bad_project_url = fb_api_path('soumya.ray.prof?fields=id%2Cname%2Cbirthday%2Cemail%2Cfriends', config)
-fb_response[bad_project_url] = call_fb_url(bad_project_url)
-fb_response[bad_project_url].parse
+bad_project_url = yt_api_path(SEARCH_KEY_WORD, 'bad_token', SEARCH_RESULT_NUM)
+yt_response[bad_project_url] = call_yt_url(bad_project_url)
+yt_response[bad_project_url].parse
 
-File.write('../spec/fixtures/fb_results.yml', fb_results.to_yaml)
+File.write('../spec/fixtures/yt_results.yml', yt_results.to_yaml)
