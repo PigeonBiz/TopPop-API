e850322ec63c18d7fcf628e38fed82bafcc2c12e
KaiyiChu <s28238385@gmail.com>
2022-10-19 21:17:50 +0800

    Rewrite to youtube api all complete

--- lib/project_info.rb
+++ lib/project_info.rb
@@ -4,8 +4,8 @@ require 'http'
 require 'yaml'
 
 config = YAML.safe_load(File.read('../config/secrets.yml'))
-SEARCH_KEY_WORD = 'taylor%20swift'
-SEARCH_RESULT_NUM = '5'
+SEARCH_KEY_WORD = 'taylor%20swift%20offical'
+SEARCH_RESULT_NUM = 5
 
 def yt_api_path(search, config, result_num)
   "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{search}&key=#{config['ACCESS_TOKEN']}&type=video&maxResults=#{result_num}"
@@ -18,20 +18,20 @@ end
 yt_response = {}
 yt_results = {}
 
-## HAPPY project request
-project_url = yt_api_path(SEARCH_KEY_WORD, config, SEARCH_RESULT_NUM)
-yt_response[project_url] = call_yt_url(project_url)
-project = yt_response[project_url].parse
+## HAPPY search request
+search_url = yt_api_path(SEARCH_KEY_WORD, config, SEARCH_RESULT_NUM)
+yt_response[search_url] = call_yt_url(search_url)
+search_result = yt_response[search_url].parse
 
-yt_results['kind'] = project['kind']
-yt_results['etag'] = project['etag']
-yt_results['nextPageToken'] = project['nextPageToken']
-yt_results['regionCode'] = project['regionCode']
-yt_results['items'] = project['items']
+yt_results['kind'] = search_result['kind']
+yt_results['etag'] = search_result['etag']
+yt_results['nextPageToken'] = search_result['nextPageToken']
+yt_results['regionCode'] = search_result['regionCode']
+yt_results['items'] = search_result['items']
 
-# BAD project request
-bad_project_url = yt_api_path(SEARCH_KEY_WORD, 'bad_token', SEARCH_RESULT_NUM)
-yt_response[bad_project_url] = call_yt_url(bad_project_url)
-yt_response[bad_project_url].parse
+# BAD search request
+bad_search_url = yt_api_path(SEARCH_KEY_WORD, 'bad_token', SEARCH_RESULT_NUM)
+yt_response[bad_search_url] = call_yt_url(bad_search_url)
+yt_response[bad_search_url].parse
 
 File.write('../spec/fixtures/yt_results.yml', yt_results.to_yaml)
