9970a0a462c2dc88e58dfb57b879366f67c9c830
jztroi <80251388+jztroi@users.noreply.github.com>
2022-12-08 00:23:07 +0800

    Add service. Not ready to use.

--- app/application/services/add_videos.rb
+++ app/application/services/add_videos.rb
@@ -4,22 +4,45 @@ require 'dry/transaction'
 
 module TopPop
   module Service
-    class AddVideos
+    class AddVideo
       include Dry::Transaction
 
-      step :add_videos
+      step :find_video
+      step :store_video
 
       private
 
       DB_ERR_MSG = 'Having trouble accessing the database'
+      DB_ADD_ERR_MSG = 'Having trouble adding video to the database'
 
-      # Add unique videos to database
-      def add_videos(videos)
-        added_video = videos.map {|video| Repository::Videos.create(video)}
-        Success(Response::ApiResult.new(status: :created, message: added_video))
+      def find_video(input)
+        if (video = video_in_database(input))
+          input[:local_video] = video
+        else
+          input[:remote_video] = video
+        end
+        Success(input)
+      rescue StandardError => e
+        Failure(Response::ApiResult.new(status: :not_found, message: DB_ERR_MSG))
+      end
+
+      def store_video(input)
+        video =
+          if (new_vid = input[:remote_video])
+            Repository::For.entity(new_vid).create(new_vid)
+          else
+            input[:local_video]
+          end
+        Success(Response::ApiResult.new(status: :created, message: video))
       rescue StandardError => e
         puts e.backtrace.join("\n")
-        Failure(Response::ApiResult.new(status: :internal_error, message: DB_ERR_MSG))
+        Failure(Response::ApiResult.new(status: :internal_error, message: DB_ADD_ERR_MSG))
+      end
+
+      # Support methods for steps
+
+      def video_in_database(video)
+        Repository::For.klass(Entity::Video).find(video)
       end
     end
   end
