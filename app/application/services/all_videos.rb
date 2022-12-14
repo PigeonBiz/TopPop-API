102533852890947e6329c971dc641aba6c86db4a
KaiyiChu <s28238385@gmail.com>
2022-12-14 23:27:25 +0800

    fix:merge error

--- app/application/services/all_videos.rb
+++ app/application/services/all_videos.rb
@@ -8,28 +8,6 @@ module TopPop
       include Dry::Transaction
 
       step :get_all_videos
-<<<<<<< HEAD
-      step :reify_videos
-
-      private
-      
-      def get_all_videos()
-        result_videos = Gateway::Api.new(TopPop::App.config)
-          .get_all_videos()
-        result_videos.success? ? Success(result_videos.payload) : Failure(result_videos.message)
-      rescue StandardError => e
-        puts e.inspect
-        puts e.backtrace
-        Failure('Cannot get videos right now; please try again later')
-      end
-
-      def reify_videos(videos_json)
-        Representer::Videos.new(OpenStruct.new)
-          .from_json(videos_json)
-          .then { |videos| Success(videos) }
-      rescue StandardError
-        Failure('Error in parsing videos; please try again later')
-=======
       step :create_video_list
 
       private
@@ -52,7 +30,6 @@ module TopPop
       rescue StandardError => e
         puts e.backtrace.join("\n")
         Failure(Response::ApiResult.new(status: :internal_error, message: PARSE_ERR_MSG))
->>>>>>> 5d0f626fbf32d631e1eb6300bc78219b54848dfc
       end
     end
   end
--- app/application/services/search_videos.rb
+++ app/application/services/search_videos.rb
@@ -7,7 +7,6 @@ module TopPop
     class SearchVideos
       include Dry::Transaction
 
-<<<<<<< HEAD
       step :get_youtube_videos
       step :create_video_list
 
@@ -33,43 +32,7 @@ module TopPop
         Success(Response::ApiResult.new(status: :ok, message: video_list))
       rescue StandardError => e
         puts e.backtrace.join("\n")
-<<<<<<< HEAD
-        Failure(Response::ApiResult.new(status: :internal_error, message: LIST_ERR_MSG))
-=======
-      step :verify_input
-      step :get_youtube_videos
-      step :reify_videos
-
-      private
-
-      def verify_input(input)
-        if input.success?
-          Success(input)
-        else
-          Failure(input.errors.values.join('; '))
-        end
-      end
-
-      def get_youtube_videos(input)
-        result_videos = Gateway::Api.new(TopPop::App.config)
-          .search_videos(input[:search_keyword])
-        result_videos.success? ? Success(result_videos.payload) : Failure(result_videos.message)
-      rescue StandardError => e
-        puts e.inspect
-        puts e.backtrace
-        Failure('Cannot get videos right now; please try again later')
-      end
-
-      def reify_videos(videos_json)
-        Representer::Videos.new(OpenStruct.new)
-          .from_json(videos_json)
-          .then { |videos| Success(videos) }
-      rescue StandardError
-        Failure('Error in parsing videos; please try again later')
->>>>>>> a2440d7d58116bd0838227d3a9b072554878a1ad
-=======
         Failure(Response::ApiResult.new(status: :internal_error, message: PARSE_ERR_MSG))
->>>>>>> 5d0f626fbf32d631e1eb6300bc78219b54848dfc
       end
     end
   end
